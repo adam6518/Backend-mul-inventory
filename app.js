4a8d2f56951ae0f04c92c3009a7e5a98dad7e6f2
adam6518 <getamped75@gmail.com>
2025-05-14 00:45:21 +0700

    finish 1

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-import express from 'express';
-import {checkConnection} from './src/config/db.js'
-import authRoutes from './src/routes/authRoutes.js'
-import projectRoutes from './src/routes/projectRoutes.js'
-import userRoutes from './src/routes/userRoutes.js'
-import orderRoutes from './src/routes/orderRoutes.js'
-import riwayatRoutes from './src/routes/riwayatRoutes.js'
-import finansialRoutes from './src/routes/finansialRoutes.js'
-import cors from 'cors'
+import express from "express";
+import { checkConnection } from "./src/config/db.js";
+import authRoutes from "./src/routes/authRoutes.js";
+import projectRoutes from "./src/routes/projectRoutes.js";
+import userRoutes from "./src/routes/userRoutes.js";
+import orderRoutes from "./src/routes/orderRoutes.js";
+import riwayatRoutes from "./src/routes/riwayatRoutes.js";
+import finansialRoutes from "./src/routes/finansialRoutes.js";
+import cors from "cors";
 
-import createAllTable from './src/utils/dbUtils.js' 
+import createAllTable from "./src/utils/dbUtils.js";
 
 const PORT = process.env.PORT || 3000;
+const corsOptions = {
+  origin: "https://backend-mul-inventory-production.up.railway.app",
+  credentials: true,
+};
 const app = express();
-app.use(cors())
+app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/project', projectRoutes);
-app.use('/api/order', orderRoutes);
-app.use('/api/riwayat', riwayatRoutes);
-app.use('/api/finansial', finansialRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/project", projectRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/riwayat", riwayatRoutes);
+app.use("/api/finansial", finansialRoutes);
 
 app.listen(PORT, async () => {
-    console.log(`Server rnning on port ${PORT}`);
-    try {
-        await checkConnection()
-        await createAllTable()
-    } catch(error) {
-        console.log('failed to initiate databases',error);
-        
-    }
+  console.log(`Server rnning on port ${PORT}`);
+  try {
+    await checkConnection();
+    await createAllTable();
+  } catch (error) {
+    console.log("failed to initiate databases", error);
+  }
 });
