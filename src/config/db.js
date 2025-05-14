68473307426c1fcd7ad111517f0c22305a909b64
adam6518 <getamped75@gmail.com>
2025-05-15 01:39:03 +0700

    finish 1

--- src/config/db.js
+++ src/config/db.js
@@ -1,27 +1,28 @@
-import mysql2 from 'mysql2/promise';
-import dotenv from 'dotenv';
+import mysql2 from "mysql2/promise";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const pool = mysql2.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10,
-    queueLimit: 0,
-    waitForConnections: true,
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 10,
+  queueLimit: 0,
+  waitForConnections: true,
+});
 
-const checkConnection = async() => {
-    try {
-        const connection = await pool.getConnection();
-        console.log('Database connection established successfully');
-        connection.release();
-    } catch (error) {
-        console.error('Error connecting to the database:', error);
-        throw error;
-    }
-}
+const checkConnection = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Database connection established successfully");
+    connection.release();
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw error;
+  }
+};
 
-export {pool, checkConnection}
+export { pool, checkConnection };
