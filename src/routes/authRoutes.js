7b8b12fa5b69cff2b2e16a0ba4bf294791b12acd
adam6518 <getamped75@gmail.com>
2025-05-14 23:42:57 +0700

    finish 1

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,11 @@ import { register, login } from "../controllers/authController.js";
 const router = express.Router();
 
 router.post('/register-user', register)
-router.post('/login-user', login)
+// router.post('/login-user', login)
+router.post('/login-user', (req, res, next) => {
+    console.log('ROUTE login-user terpanggil');
+    next(); // lanjut ke controller
+  }, login);
+  
 
 export default router;
