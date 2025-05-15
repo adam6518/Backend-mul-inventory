1bc0b1ee31ba5e9ec26a1afc67e1ef9c431eb109
adam6518 <getamped75@gmail.com>
2025-05-15 23:59:26 +0700

    finish 1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import cors from "cors";
 import createAllTable from "./src/utils/dbUtils.js";
 
 const app = express();
-const PORT = process.env.DB_PORT || 3306;
+const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
@@ -31,10 +31,10 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Boleh true walaupun tidak pakai cookie
 };
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
